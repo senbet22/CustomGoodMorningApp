1cca518fee38359ac315874543ed113bafdcc7a1
senbet22 <senbet22@users.noreply.github.com>
2026-04-08 11:08:33 +0200

    Based on time of day key is chosen, and based on key message(value) displayed from dictionary.

--- Program.cs
+++ Program.cs
@@ -2,31 +2,29 @@
 Console.WriteLine("///////////////////////////////");
 
 Console.WriteLine("Please enter your name!");
-string userInputName = Console.ReadLine() ?? "";
+string name = Console.ReadLine() ?? "";
 
 
-Console.WriteLine("and now your age...");
-string userInputAge = Console.ReadLine()?? "";
+int timeOfDay = DateTime.Now.Hour;
 
 
-if (!int.TryParse(userInputAge, out int age))
+Dictionary<string, string> greetings = new Dictionary<string, string>
 {
-    Console.WriteLine("Invalid input");
-}
-
-
-
-Console.WriteLine($"Good morning {userInputName}, You are {userInputAge} years old");
-
-
-
-
-
-Dictionary<string, string> myDictionary = new Dictionary<string, string>
-{
-    { "morgen", "God morgen!"},
-    { "formiddag", "God Formiddag!"},
-    { "ettermiddag", "God ettermiddag!"},
-    { "kveld", "God kveld!"},
-    { "natt", "Hei! Det er ikke så sunt for deg å være oppe så lenge."}
+    { "morgen", $"God morgen, {name}!"},
+    { "formiddag", $"God Formiddag, {name}!"},
+    { "ettermiddag", $"God ettermiddag, {name}!"},
+    { "kveld", $"God kveld, {name}!"},
+    { "natt", $"Hei {name}, Det er kanskje på tide å legge seg."}
 };
+
+
+string key = timeOfDay switch
+{
+    >= 6 and < 9 => "morgen",
+    >= 9 and < 12 => "formiddag",
+    >= 12 and < 18 => "ettermiddag",
+    >= 18 and < 22 => "kveld",
+    _ => "natt"
+};
+
+Console.WriteLine(greetings[key]);
